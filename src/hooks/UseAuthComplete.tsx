e8d6c8e72b10dd4195984ce1fe7ffde9e2ab0adb
dorcax <dorcasibrahim667@gmail.com>
2026-02-11 13:49:20 +0100

    fixed the dashboard

--- src/hooks/UseAuthComplete.tsx
+++ src/hooks/UseAuthComplete.tsx
@@ -1,73 +1,26 @@
-import { Role, type AuthState } from '@/api/api.type'
-import { useAuthState } from '@/api/data/auth'
-import { useCallback } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { Role, type AuthState } from '@/api/api.type';
+import { useAuthState } from '@/api/data/auth';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const UseAuthComplete = () => {
-    const auth =useAuthState()
-    const navigate =useNavigate()
-    const {search} =useLocation()
-  return useCallback(
-    async(result:Promise<AuthState>)=>{
-        const res =await result 
-        console.log("Logging in now:", res);
-        console.log("loginging",res)
-        auth.set(res)
-          // CRITICAL FIX: Add a small delay to ensure cookie is stored
-            await new Promise(resolve => setTimeout(resolve, 300));
-        if(res.role ===Role.GUEST){
-            navigate('/dashboard',{replace:true})
-        }else{
-            navigate("/",{replace:true})
-        }
-        return res
-     
-    }
-  ,[auth,search,navigate])
-}
+  const auth = useAuthState();
+  const navigate = useNavigate();
+
+  return useCallback(async (loginPromise: Promise<AuthState>) => {
+    const res = await loginPromise;
+
+    console.log("Logging in now:", res);
+
+    auth.set(res);
+
+    if (res.role === Role.GUEST) navigate('/dashboard', { replace: true });
+    else navigate('/', { replace: true });
+
+    return res;
+  }, [auth, navigate]);
+};
 
 export default UseAuthComplete
-
-
-
-
-// import { Role, type AuthState } from "@/api/api.type";
-// import { useAuthState } from "@/api/data/auth";
-// import { useCallback } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { envUrl } from "@/api/data/base";
-
-// const UseAuthComplete = () => {
-//   const auth = useAuthState();
-//   const navigate = useNavigate();
-
-//   return useCallback(
-//     async (loginPromise: Promise<AuthState>) => {
-//       const res = await loginPromise; // original login mutation
-// console.log("Initial login result:", res);
-// console.log("env",envUrl)
-//       // ✅ Wait for backend to confirm cookie
-//         auth.set(res)
-//       // const meRes = await fetch(`${envUrl}/auth`, {
-//       //   credentials: "include", // important
-//       // }).then((r) => r.json());
-
-//       // // Set auth state with confirmed data
-//       // auth.set(meRes);
-
-//       // Navigate based on role
-//       if (res.role === Role.GUEST) {
-//         navigate("/dashboard", { replace: true });
-//       } else {
-//         navigate("/", { replace: true });
-//       }
-
-//       return res;
-//     },
-//     [auth, navigate]
-//   );
-// };
-
-// export default UseAuthComplete;
-
-
